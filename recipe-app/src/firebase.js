2d1ab05f188e9f3c55d5a7bd3f29d5339591172e
Jared <jaredkohou@gmail.com>
2021-04-12 21:45:32 -0500

    js

--- recipe-app/src/firebase.js
+++ recipe-app/src/firebase.js
@@ -3,13 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDaAeYSbadW_hmtO9d8h_70VOtS6e4IMmk",
-  authDomain: "recipe-app-challenge.firebaseapp.com",
-  databaseURL: "https://recipe-app-challenge.firebaseio.com",
-  projectId: "recipe-app-challenge",
-  storageBucket: "recipe-app-challenge.appspot.com",
-  messagingSenderId: "52070158457",
-  appId: "1:52070158457:web:239fb73767a69b07b284a8",
+    apiKey: "AIzaSyB0BtRgQ0UK_Ac8ooHSVWOEJSEtkJVEalA",
+    authDomain: "my-recipe-app-5fde2.firebaseapp.com",
+    projectId: "my-recipe-app-5fde2",
+    storageBucket: "my-recipe-app-5fde2.appspot.com",
+    messagingSenderId: "428624181194",
+    appId: "1:428624181194:web:0edb43f7485e2fcf294133"
 };
 
 firebase.initializeApp(firebaseConfig);
